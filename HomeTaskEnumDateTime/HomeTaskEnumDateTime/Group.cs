67afe538cb7c9ed6b148e0b884aa32c6f1159d2a
KhalidSuleymanov <khalidms@code.edu.az>
2023-02-07 00:12:05 +0400

    Khalid

---
+++ HomeTaskEnumDateTime/HomeTaskEnumDateTime/Group.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeTaskEnumDateTime
+{
+    internal class Group
+    {
+        public string No;
+        public GroupType Type;
+        public DateTime StartDate;
+    }
+}
